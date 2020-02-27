93c86506a21ac678205bd07e12968ceb2ea62a60
Simone Sala <simonesala@me.com>
2020-02-27 13:49:55 +0000

    Consegna

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$("header .fas.fa-bars").click(function(){
+    $(".hamburger-menu").addClass("active");
+});
+$("header .close").click(function(){
+    $(".hamburger-menu").removeClass("active");
+});
