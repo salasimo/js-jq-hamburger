d11613fcbc2919bb4e1c20444cd4a1fde7546f93
Simone Sala <simonesala@me.com>
2020-02-28 09:13:47 +0000

    Toggle

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
-$("header .fas.fa-bars").click(function(){
-    $(".hamburger-menu").addClass("active");
-});
-$("header .close").click(function(){
-    $(".hamburger-menu").removeClass("active");
+// $("header .fas.fa-bars").click(function(){
+//     $(".hamburger-menu").addClass("active");
+// });
+// $("header .close").click(function(){
+//     $(".hamburger-menu").removeClass("active");
+// });
+
+// USANDO TOGGLE=======================================
+
+$("header .fas.fa-bars, header .close").click(function(){
+    $(".hamburger-menu").toggleClass("active");
 });
